6bfd3d54176bc0944000473e91ad65bdd3301802
Asraful-code235 <asrafulislamcodes@gmail.com>
2021-09-22 00:14:45 +0600

    comitted

--- src/slider.js
+++ src/slider.js
@@ -1,28 +1,28 @@
 export const sliderInfo = [
   {
     id: 1,
-    cName: "slide active",
-    img: "./Bookshelfs.jpg",
+    cName: "slide ",
+    img: "./1.jpg",
     h3: "Getting Books Made Easy",
     p: "Get your favourite book with just a click of a button",
   },
   {
     id: 2,
-    cName: "slide",
+    cName: "slide ",
     img: "./differentbooks.jpg",
     h3: "Book Of Different Kinds",
     p: "We Have Every Book That You Can Think Of",
   },
   {
     id: 3,
-    cName: "slide",
+    cName: "slide ",
     img: "./Personreading.jpg",
     h3: "Easy Accessable learning experience",
     p: "Download pdf or complete book",
   },
   {
     id: 4,
-    cName: "slide",
+    cName: "slide ",
     img: "./py.jpg",
     h3: "Learn New Technologies",
     p: "Get along with the current hot topics",
